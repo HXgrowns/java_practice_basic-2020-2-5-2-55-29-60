5eecd832df7e8f72e808e7d060cff0f6a27ff1dd
huxiao <804156017@qq.com>
2020-02-06 17:59:16 +0800

    [huxiao] {add}: add bestCharge function

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -1,5 +1,8 @@
 package com.thoughtworks;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class App {
@@ -18,8 +21,126 @@ public class App {
    * @param selectedItems 选择的菜品信息
    */
   public static String bestCharge(String selectedItems) {
-    // 此处补全代码
-    return selectedItems;
+    Map<String, Integer> map = new LinkedHashMap<>();
+    map = parseParameter(selectedItems);
+    String[] itemIds = getItemIds();
+    String[] itemNames = getItemNames();
+    double[] itemPrices = getItemPrices();
+    double total = 0.0;
+    double discountFull = 0.0;
+    double discountHalf = 0.0;
+    String result = "============= 订餐明细 =============\n";
+    for (String key : map.keySet()) {
+      for (int i = 0; i < itemIds.length; i++) {
+        if(key.equals(itemIds[i])) {
+          Integer value = map.get(key);
+          total += calculateEverMoney(value, itemPrices[i]);
+          discountHalf += discountHalfReduce(key, value, itemPrices[i]);
+          result += printSection1(itemNames[i], value, (int)calculateEverMoney(value, itemPrices[i]));
+          break;
+        }
+      }
+    }
+    discountFull = discountFullReduce(total);
+
+    if (discountFull > discountHalf) {
+      result += printSection2("指定菜品半价(黄焖鸡，凉皮)", (int)(total - discountHalf));
+      result += printSection3((int)discountHalf);
+    } else if (discountFull < discountHalf) {
+      result += printSection2("满30减6元", (int)(total - discountFull));
+      result += printSection3((int)discountFull);
+    } else {
+      result += printSection3((int)total);
+    }
+    return result;
+  }
+
+  /**
+   * 解析参数，将菜品ID当做key，菜品数量当做value存入map
+   * @param selectedItems
+   * @return
+   */
+  public static Map<String, Integer> parseParameter(String selectedItems) {
+    String[] parameters = selectedItems.split(",");
+
+    Map<String, Integer> map = new LinkedHashMap<>();
+    for (String parameter : parameters) {
+      String[] items = parameter.split(" x ");
+      String key = items[0];
+      Integer value = Integer.valueOf(items[1]);
+      map.put(key,value);
+    }
+    return map;
+  }
+
+  /**
+   * 计算每个物品的价钱
+   * @param itemCount
+   * @param itemPrice
+   * @return
+   */
+  public static double calculateEverMoney(Integer itemCount, double itemPrice) {
+    return itemCount * itemPrice;
+  }
+
+  /**
+   * 计算满30减6元的优惠价格
+   * @param itemsMoney
+   * @return
+   */
+  public static double discountFullReduce(double itemsMoney) {
+    return itemsMoney - Math.floor(itemsMoney / 30) * 6;
+  }
+
+  /**
+   * 计算指定商品半价的优惠价格
+   * @param itemId
+   * @param itemCount
+   * @param itemPrice
+   * @return
+   */
+  public static double discountHalfReduce(String itemId, Integer itemCount, double itemPrice) {
+    String[] halfPrices = getHalfPriceIds();
+    for (int i = 0; i < halfPrices.length; i++) {
+      if (itemId.equals(halfPrices[i])) {
+        return itemCount * itemPrice / 2;
+      }
+    }
+    return itemCount * itemPrice;
+  }
+
+  /**
+   * 拼接购买商品列表字符串
+   * @param itemName
+   * @param itemCount
+   * @param itemMoney
+   * @return
+   */
+  public static String printSection1(String itemName, Integer itemCount, int itemMoney) {
+    return itemName + " x " + itemCount + " = " + itemMoney + "元\n";
+  }
+
+  /**
+   * 拼接优惠方式字符串
+   * @param discountMathod
+   * @param reduceMoney
+   * @return
+   */
+  public static String printSection2(String discountMathod, int reduceMoney) {
+    return "-----------------------------------\n"
+            + "使用优惠:\n"
+            + discountMathod + "，省" + reduceMoney + "元\n";
+  }
+
+  /**
+   * 拼接总计字符串
+   * @param discount
+   * @return
+   */
+  public static String printSection3(int discount) {
+    return "-----------------------------------\n"
+            + "总计：" + discount +"元\n"
+            + "===================================";
   }
 
   /**
